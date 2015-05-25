0f087aeed8b009aff8000c392ebe185a58480269
xiangormirko <xiangormirko@gmail.com>
2015-05-25 18:04:39 +0200

    Initial Commit

---
+++ Pitch Perfect 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pitch Perfect 2
+//
+//  Created by MIRKO on 5/25/15.
+//  Copyright (c) 2015 XZM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
